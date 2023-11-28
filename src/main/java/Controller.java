572c42c00e3c858f53e797244c178e3b36170f9a
jules0178 <juma0002@stud.kea.dk>
2023-11-28 10:48:38 +0100

    Merged branch (chanching switch statement) into main and removed out commented code in controller

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -28,18 +28,6 @@ public class Controller {
             }
         }
     }
-   /* private void assignSwimmersToTeams() {
-        for ( Member swimmer  : memberDatabase.getMembersArrayList()) {
-            int age = swimmer.calculateAge();
-            if (age < 18) {
-                ((Swimmer) swimmer).setTeam(juniorTeam);
-                juniorTeam.addMember(swimmer);
-            } else {
-                ((Swimmer) swimmer).setTeam(seniorTeam);
-                seniorTeam.addMember(swimmer);
-            }
-        }
-    }*/
 
     public void addMember(String name, String surName, String email, int phoneNumber, String dateOfBirth, String dateJoined, boolean isActive, boolean isCompetitor) {
         memberDatabase.addMember(name, surName, email, phoneNumber, dateOfBirth, dateJoined, isActive, isCompetitor);
