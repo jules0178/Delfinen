9e7ea75d9cbab85fe1bb3fa90b1cee97e5b523a4
jules0178 <juma0002@stud.kea.dk>
2023-11-28 14:35:46 +0100

    Fixed missing code from merge

--- src/main/java/Swimmer.java
+++ src/main/java/Swimmer.java
@@ -7,14 +7,21 @@ public class Swimmer extends Member {
     public List<Result> results;
 
     public List<Result> practice;
+    public Team team;
 
     public Swimmer(String name, String surName, String email, int phoneNumber, String dateOfBirth, String dateJoined, boolean isActive, boolean isCompetitor, String memberID) {
         super(name, surName, email, phoneNumber, dateOfBirth, dateJoined, isActive, isCompetitor, memberID);
         this.results = new ArrayList<>();
         this.practice = new ArrayList<>();
-
+        this.team = null;
     }
-    public List<Result> getPractice() {
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+    public Team getTeam() {
+        return team;
+
+    }    public List<Result> getPractice() {
         return practice;
     }
 
